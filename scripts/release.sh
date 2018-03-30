44c4fbf093e47caac82d1d2c7cb3dc24527db391
zhouqing <zhouqing2@aixuedai.com>
2018-03-30 14:26:04 +0800

    releas

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,12 +11,14 @@ read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 echo "Releasing $VERSION ..."
-echo "Releasing $RELEASE_TAG ..."
+echo "tag $RELEASE_TAG ..."
     # build
     # VERSION=$VERSION npm run build
-    echo 'build done---'
+    # updata version
+    npm version $VERSION
     # commit
     git add -f \
         dist/*.js
     git commit -m "build: build $VERSION"
+    git push
 fi
