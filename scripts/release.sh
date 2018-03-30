74c110e95ec64c9f6d6ea30653425d0f2a824c73
zhouqing <zhouqing2@aixuedai.com>
2018-03-30 14:29:34 +0800

    rel

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,24 +1,29 @@
 set -e
 
-if [[ -z $1 ]]; then
-echo "Enter new version: "
-read VERSION
-else
-VERSION=$1
-fi
+# if [[ -z $1 ]]; then
+# echo "Enter new version: "
+# read VERSION
+# else
+# VERSION=$1
+# fi
 
-read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-echo "Releasing $VERSION ..."
-echo "tag $RELEASE_TAG ..."
-    # build
-    # VERSION=$VERSION npm run build
-    # updata version
-    npm version $VERSION
-    # commit
-    git add -f \
-        dist/*.js
-    git commit -m "build: build $VERSION"
-    git push
-fi
+# read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+# echo
+# if [[ $REPLY =~ ^[Yy]$ ]]; then
+# echo "Releasing $VERSION ..."
+# echo "tag $RELEASE_TAG ..."
+#     # build
+#     # VERSION=$VERSION npm run build
+#     # updata version
+#     npm version $VERSION
+#     # commit
+#     git add -f \
+#         dist/*.js
+#     git commit -m "build: build $VERSION"
+#     git push
+# fi
+
+git add -f \
+    dist/*.js
+git commit -m "auto build"
+git push
