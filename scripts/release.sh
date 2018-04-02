23d87dbc6608de7fd90d6862cf181e46870e4a81
zhouqing <zhouqing2@aixuedai.com>
2018-04-02 11:01:24 +0800

    x

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,30 +1,31 @@
 set -e
 
-# if [[ -z $1 ]]; then
-# echo "Enter new version: "
-# read VERSION
-# else
-# VERSION=$1
-# fi
+if [[ -z $1 ]]; then
+echo "Enter new version: "
+read VERSION
+else
+VERSION=$1
+fi
 
-# read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
-# echo
-# if [[ $REPLY =~ ^[Yy]$ ]]; then
-# echo "Releasing $VERSION ..."
-# echo "tag $RELEASE_TAG ..."
-#     # build
-#     # VERSION=$VERSION npm run build
-#     # updata version
-#     npm version $VERSION
-#     # commit
-#     git add -f \
-#         dist/*.js
-#     git commit -m "build: build $VERSION"
-#     git push
-# fi
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+echo "Releasing $VERSION ..."
+echo "tag $RELEASE_TAG ..."
+    # build
+    VERSION=$VERSION npm run build
+    # updata version
+    npm version $VERSION
+    # commit
+    git add -A
+    git add -f \
+        dist/*.js
+    # git commit -m "build: build $VERSION"
+    # git push
+fi
 
-echo "tag is $1 $TRAVIS_BRANCH"
-git add -f \
-    lib/**/*
-git commit -m "auto build"
-git push origin $TRAVIS_BRANCH
+# echo "tag is   $RELEASE_TAG"
+# git add -f \
+#     lib/**/*
+# git commit -m "auto build"
+# git push origin $TRAVIS_BRANCH
