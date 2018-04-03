1d20c9d77e91a88690cba7ae17610a99250dfd3f
zhouqing <zhouqing2@aixuedai.com>
2018-04-03 11:58:03 +0800

    build: build 1.1.16

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-importScripts('workbox-sw.prod.v2.1.2.js');
+importScripts('workbox-sw.prod.v2.1.3.js');
 
 /**
  * DO NOT EDIT THE FILE MANIFEST ENTRY
@@ -20,119 +20,119 @@ importScripts('workbox-sw.prod.v2.1.2.js');
 const fileManifest = [
   {
     "url": "document.html",
-    "revision": "a85cae9f8024a233865cb80e593d8fb8"
+    "revision": "5c8aa3241e79ab80dc7b886e288362a4"
   },
   {
     "url": "examples.html",
-    "revision": "6fc387d1a034b5f432ae2a9a7cf0072f"
+    "revision": "688272d1b7872de788600c930dc79753"
   },
   {
-    "url": "static/css/document.7d6743b.css",
-    "revision": "90fd077ff29ae5c4396452afeac12086"
+    "url": "static/css/document.017564a.css",
+    "revision": "45fd85870a5c2e081f460a5c0764d38c"
   },
   {
     "url": "static/css/examples.6dade68.css",
     "revision": "6046b4b8d82f3b548561a537705ab195"
   },
   {
-    "url": "static/js/0.dda7cf9.js",
-    "revision": "5f63581cce5f960c6cc198be4a301367"
+    "url": "static/js/0.3c1f8df.js",
+    "revision": "c65fe47ebf7b51cb3a1f4fe6d49f1f3d"
   },
   {
-    "url": "static/js/1.bd6bf3b.js",
-    "revision": "ea894307aebe9a0d74310ee99296568c"
+    "url": "static/js/1.4473c9e.js",
+    "revision": "243abefd84996c3c5d832c51d23b0836"
   },
   {
-    "url": "static/js/10.682ae8b.js",
-    "revision": "8ed781188625a80d3af30ebbcc261f40"
+    "url": "static/js/10.c79aa86.js",
+    "revision": "c968598cd3b6c68e0aa25497f7cf3caf"
   },
   {
-    "url": "static/js/11.bd896a7.js",
-    "revision": "533b8d556b3ae6458ac295c65908b99a"
+    "url": "static/js/11.bc17067.js",
+    "revision": "21a8d876da3876c0f003d4644ef3552c"
   },
   {
-    "url": "static/js/12.72d4168.js",
-    "revision": "29723759f00d8fb1583ef3522bab8705"
+    "url": "static/js/12.5a46e54.js",
+    "revision": "daa850f8455e5fdd5b468af61e6405ad"
   },
   {
-    "url": "static/js/13.7f00282.js",
-    "revision": "d845a3818d67b65e3d4f30a73bd0f642"
+    "url": "static/js/13.11e3c0f.js",
+    "revision": "82b575a28f35b0a80774c48150b4a4ec"
   },
   {
-    "url": "static/js/14.7a2ffde.js",
-    "revision": "22a956c9f08c1cdefab92686f838ff5e"
+    "url": "static/js/14.5be6d17.js",
+    "revision": "1e43f92bf5e8f65c3348a37601d456d4"
   },
   {
-    "url": "static/js/15.429686c.js",
-    "revision": "b974988ebf4383c3e6bde1afce9920f9"
+    "url": "static/js/15.82316c6.js",
+    "revision": "b9fb68bfee2453fb788a246b36897e14"
   },
   {
-    "url": "static/js/16.fe7c392.js",
-    "revision": "339de9e303c1a370aa2707eab703e5a1"
+    "url": "static/js/16.662237f.js",
+    "revision": "72fc23e6921a22297ca96951e9d6bdae"
   },
   {
-    "url": "static/js/17.905d566.js",
-    "revision": "24a00f36576cd71c456a0c01209c9a9d"
+    "url": "static/js/17.2924e16.js",
+    "revision": "c83f3d52342ca804e773a3070bd03dc0"
   },
   {
-    "url": "static/js/18.3899818.js",
-    "revision": "fc3df5ae458f26e0c992cd81af310ec2"
+    "url": "static/js/18.6a6a306.js",
+    "revision": "95ffe2e1694d770c5b04be7171eaedda"
   },
   {
-    "url": "static/js/19.bfeda4d.js",
-    "revision": "9b9feadae8ed50690d1aab89d8732976"
+    "url": "static/js/19.fac64ce.js",
+    "revision": "3f2e2e285816aa9fb65d43033c713674"
   },
   {
-    "url": "static/js/2.3aba921.js",
-    "revision": "4704ca3bf9d420e11b0a03cbca370625"
+    "url": "static/js/2.3ec4d9f.js",
+    "revision": "cb89cc813dffa9701c5025b60c4883ef"
   },
   {
-    "url": "static/js/20.9a7ee8c.js",
-    "revision": "4dcbc0776582fe311487ddd27b541294"
+    "url": "static/js/20.512e0f4.js",
+    "revision": "79802a3494e0f26408ded745b7e43d85"
   },
   {
-    "url": "static/js/3.3080ded.js",
-    "revision": "f325b54b2e97838410ed0af5c62cbbbb"
+    "url": "static/js/3.a61a9cd.js",
+    "revision": "b898ef0f859bdf8bf78ed3258a854f16"
   },
   {
-    "url": "static/js/4.39ef261.js",
-    "revision": "7915a900da1e6b6f3a62ab5a66e7563b"
+    "url": "static/js/4.53f6aa5.js",
+    "revision": "beb9cbda379bac3ef5751ae592017bf2"
   },
   {
-    "url": "static/js/5.b9f6a70.js",
-    "revision": "b4daa5b0580c85d4187a3140d655a48c"
+    "url": "static/js/5.2209f32.js",
+    "revision": "f8f7c5a5783026854c0b477f0f1ed611"
   },
   {
-    "url": "static/js/6.d3f2bcf.js",
-    "revision": "c947870fb297a0a1b6e420580a27688b"
+    "url": "static/js/6.e9663d5.js",
+    "revision": "5aa0b03a6ac39085065775c88ea52eaf"
   },
   {
-    "url": "static/js/7.edad1bc.js",
-    "revision": "cc656e4316e3c974c051709b1e9b2ab0"
+    "url": "static/js/7.a258021.js",
+    "revision": "4f814e75aeaff33519d25824cad871e7"
   },
   {
-    "url": "static/js/8.d38c411.js",
-    "revision": "7a548533f97e1872021a3471db3a0936"
+    "url": "static/js/8.ca4be9e.js",
+    "revision": "eca5f3c148b79712974d6827ef0bcf0c"
   },
   {
-    "url": "static/js/9.b79ad8b.js",
-    "revision": "418248c8341bf615e8b354131fae78ad"
+    "url": "static/js/9.c6bd98e.js",
+    "revision": "6f2222bdc2b903ea3f105d7327922cda"
   },
   {
-    "url": "static/js/document.7a54e7d.js",
-    "revision": "f8206d5850a23287f5d70f08d2660729"
+    "url": "static/js/document.3cd9538.js",
+    "revision": "e77fa0fa3438316c03f9bf45004187fe"
   },
   {
-    "url": "static/js/examples.0b78139.js",
-    "revision": "b6b920f44ac9be31bfdc08963a7a6c8f"
+    "url": "static/js/examples.d4922cb.js",
+    "revision": "5f977854a4539656ad9a19e718a9bb27"
   },
   {
-    "url": "static/js/manifest.d5472a6.js",
-    "revision": "c0fa97a5343272955eb2983d35ec9148"
+    "url": "static/js/manifest.34b399c.js",
+    "revision": "a662f568a431be90e7dc73f2ad40d678"
   },
   {
-    "url": "static/js/vendor.b606e37.js",
-    "revision": "d8205285f23d0e1ed487c5e52dc0bd8a"
+    "url": "static/js/vendor.ec96cfc.js",
+    "revision": "b95ef33cdda3933a2f4771a629944a1d"
   }
 ];
 
--- scripts/release.sh
+++ scripts/release.sh
@@ -13,16 +13,17 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 echo "Releasing $VERSION ..."
     # build
     VERSION=$VERSION npm run build && npm run deploy:docs
-    # updata version
-    npm version $VERSION
-    echo " $RELEASE_TAG ..."
-
     # commit
     git add -A
     git add -f \
         dist/**/* \
         lib/**/*
     git commit -m "build: build $VERSION"
+
+     # updata version
+    npm version $VERSION
+    echo " $RELEASE_TAG ..."
+
     git push origin refs/tags/v$VERSION
     git push
     # npm
