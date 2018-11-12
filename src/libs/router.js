0781d1b2f9a2301516837cd0f1ae2b1767690353
zhouqing <448340778@qq.com>
2018-11-12 16:16:59 +0800

    del log

--- src/libs/router.js
+++ src/libs/router.js
@@ -1,5 +1,4 @@
 export function go(url, $router) {
-    console.log('xx')
     if (/^javas/.test(url) || !url) return
     const useRouter = typeof url === 'object' || ($router && typeof url === 'string' && !/http/.test(url))
     if (useRouter) {
