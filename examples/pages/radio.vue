d8676efb15af23afeb7a21c4d24881f1be033119
zhouqing <448340778@qq.com>
2019-04-17 14:49:20 +0800

    feat(radio): radio 更新

--- examples/pages/radio.vue
+++ examples/pages/radio.vue
@@ -1,83 +1,101 @@
 <template>
-    <m-page>
-        <m-header slot="header"></m-header>
-        <demo-content>
-            <h5>选中的是 {{labelIndex}}</h5>
-            <m-radio-group v-model="labelIndex"
-                class="radio-group">
-                <m-radio v-for="(option,index) in radioOptions"
-                    :key="option.label"
-                    class="radio-item"
-                    v-slot="{ isActive }">
-                    <label class="radio-item__label"
-                        :class="{'radio-item__label_active':isActive}">
-                        {{option.label}}
-                    </label>
-                </m-radio>
-            </m-radio-group>
-            <br>
-            <br>
-            <h5>选中的是 {{activeIndex}}</h5>
-            <m-radio-group v-model="activeIndex">
-                <m-cell-group>
-                    <m-radio v-for="option in radioOptions"
-                        :key="option.label"
-                        v-slot="{ isActive }">
-                        <m-cell :title="option.label">
-                            <m-icon class="icon__check"
-                                v-if="isActive"
-                                slot="value"
-                                type="check"></m-icon>
-                        </m-cell>
-                    </m-radio>
-                </m-cell-group>
-            </m-radio-group>
-        </demo-content>
-    </m-page>
+  <m-page>
+    <m-header slot="header"></m-header>
+    <demo-content>
+      <h5>选中的是 {{labelIndex}}</h5>
+      <m-radio-group v-model="labelIndex"
+        class="radio-group">
+        <m-radio v-for="(option,index) in radioOptions"
+          :key="option.label"
+          class="radio-item"
+          v-slot="{ isActive }">
+          <label class="radio-item__label"
+            :class="{'radio-item__label_active':isActive}">
+            {{option.label}}
+          </label>
+        </m-radio>
+      </m-radio-group>
+      <br>
+      <br>
+      <h5>选中的是 {{activeIndex}}</h5>
+      <m-radio-group v-model="activeIndex">
+        <m-cell-group>
+          <m-radio v-for="option in radioOptions"
+            :key="option.label"
+            v-slot="{ isActive }">
+            <m-cell :title="option.label">
+              <m-icon class="icon__check"
+                v-if="isActive"
+                slot="value"
+                type="check"></m-icon>
+            </m-cell>
+          </m-radio>
+        </m-cell-group>
+      </m-radio-group>
+      <br>
+      <h5>多行选中的是 {{activeIndex}}</h5>
+      <m-radio-group v-model="activeIndex">
+        <m-cell-group>
+          <m-radio v-for="option in radioOptions"
+            :key="option.label"
+            :disabled="option.isDisable"
+            v-slot="{ isActive }">
+            <m-cell :title="option.label"
+              :subTitle="option.subTitle">
+              <m-icon class="icon__check"
+                v-if="isActive"
+                slot="value"
+                type="check"></m-icon>
+            </m-cell>
+          </m-radio>
+        </m-cell-group>
+      </m-radio-group>
+    </demo-content>
+  </m-page>
 </template>
 <script>
 export default {
-    name: 'demo-radio',
-    data() {
-        return {
-            labelIndex: 1,
-            activeIndex: 1,
-            radioOptions: [
-                { label: '张良', value: '1' },
-                { label: '妲己', value: '2' },
-                { label: '小乔', value: '3' }
-            ]
-        }
+  name: 'demo-radio',
+  data() {
+    return {
+      labelIndex: 1,
+      activeIndex: 1,
+      radioOptions: [
+        { label: '张良', value: '1', subTitle: '人和神说话，是信仰', isDisable: true },
+        { label: '妲己', value: '2', subTitle: '一直爱主人，因为被设定成这样' },
+        { label: '小乔', value: '3', subTitle: '要努力变强' }
+      ]
     }
+  }
 }
 </script>
 <style lang="scss"
-    scoped>
+  scoped>
 .radio-group {
-    background-color: #fff;
-    padding: px2rem(40);
+  background-color: #fff;
+  padding: px2rem(40);
 }
 
 .radio-item {
-    display: inline-block;
+  display: inline-block;
 }
 
 .radio-item__label {
-    font-size: px2rem(28);
-    padding: px2rem(10) px2rem(40);
-    color: $textColorImportant;
-    background-color: $bgColorDefault;
-    border-radius: px2rem(12);
-    margin-right: px2rem(20);
+  font-size: px2rem(28);
+  padding: px2rem(10) px2rem(40);
+  color: $textColorImportant;
+  background-color: $bgColorDefault;
+  border-radius: px2rem(12);
+  margin-right: px2rem(20);
 
-    &.radio-item__label_active {
-        color: $textBtnColor;
-        background-color: $colorPrimary;
-    }
+  &.radio-item__label_active {
+    color: $textBtnColor;
+    background-color: $colorPrimary;
+  }
 }
 
 .icon__check {
-    font-size: 14px;
-    color: $tipsColor;
+  // font-size: 16px;
+  // color: $tipsColor;
 }
 </style>
--- src/components/radio/radio.vue
+++ src/components/radio/radio.vue
@@ -1,54 +1,62 @@
 <template>
-    <div @click="onItemClick">
-        <slot :isActive="isActive"></slot>
-    </div>
+  <div class="mui-radio"
+    @click="onItemClick">
+    <slot :isActive="isActive"></slot>
+  </div>
 </template>
 <script>
 export default {
-    name: 'm-radio',
-    inject: {
-        radioGroup: {
-            default: () => null
-        }
-    },
-    props: {},
-    data() {
-        return {}
-    },
-    computed: {
-        parent() {
-            if (process.env.NODE_ENV !== 'production' && !this.radioGroup) {
-                console.error('[aym-ui] radio 需要在父组件radioGroup下')
-            }
-            return this.radioGroup
-        },
-        radioId() {
-            return `radio_${this._uid}`
-        },
-        index() {
-            if (!this.parent) return ''
-            return this.parent.items.indexOf(this.radioId)
-        },
-        isActive() {
-            return this.index === this.parent.activeIndex
-        }
-    },
-    created() {
-        if (!this.parent) return
-        this.parent.items.push(this.radioId)
-    },
-    destroyed() {
-        if (!this.parent) return
-        this.parent.items.splice(this.index, 1)
-    },
-    methods: {
-        onItemClick(event) {
-            if (this.isActive) return
-            this.parent.$emit('change', this.index)
-            this.$emit('click', event)
-            // this.routerGo()
-        }
+  name: 'm-radio',
+  inject: {
+    radioGroup: {
+      default: () => null
     }
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    parent() {
+      if (process.env.NODE_ENV !== 'production' && !this.radioGroup) {
+        console.error('[aym-ui] radio 需要在父组件radioGroup下')
+      }
+      return this.radioGroup
+    },
+    radioId() {
+      return `radio_${this._uid}`
+    },
+    index() {
+      if (!this.parent) return ''
+      return this.parent.items.indexOf(this.radioId)
+    },
+    isActive() {
+      return this.index === this.parent.activeIndex
+    }
+  },
+  created() {
+    if (!this.parent) return
+    this.parent.items.push(this.radioId)
+  },
+  destroyed() {
+    if (!this.parent) return
+    this.parent.items.splice(this.index, 1)
+  },
+  methods: {
+    onItemClick(event) {
+      if (this.isActive) return
+      if (!this.disabled) {
+        this.parent.$emit('change', this.index)
+      }
+      this.$emit('click', event)
+      // this.routerGo()
+    }
+  }
 }
 </script>
 <style lang="scss">
