3edad73d61392f41f965eaf96326b94e84708c5c
zhouqing <448340778@qq.com>
2019-04-22 10:56:31 +0800

    feat(tabbar): 增加位置属性

--- src/components/tabbar/tabbar.vue
+++ src/components/tabbar/tabbar.vue
@@ -1,47 +1,67 @@
 <template>
+  <div class="m-tabbar__outter"
+    :class="[barPosition,safeBottom]">
     <div class="m-tabbar">
-        <slot></slot>
+      <slot></slot>
     </div>
+  </div>
 </template>
 <script>
 export default {
-
-    name: 'm-tabbar',
-    model: {
-        prop: 'value',
-        event: 'change'
+  name: 'm-tabbar',
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    value: {
+      type: Number,
+      default: 0
     },
-    props: {
-        value: {
-            type: Number,
-            default: 0
-        }
-    },
-    data() {
-        return {
-            items: []
-        }
-    },
-    watch: {
-        value(to, from) {
-            this.setActiveItem()
-        }
-    },
-    mounted() {
-        this.setActiveItem()
-    },
-
-    methods: {
-        setActiveItem() {
-            this.items.forEach((item, index) => {
-                item.isActive = index === this.value
-            })
-        },
-        onChange(active) {
-            // this.$emit('input', active)
-            this.$emit('change', active)
-        }
+    fixed: {
+      type: [String, Boolean],
+      default: 'bottom',
+      validator: function(value) {
+        return ['top', 'bottom', false].indexOf(value) !== -1
+      }
     }
+  },
+  data() {
+    return {
+      items: []
+    }
+  },
+  computed: {
+    barPosition() {
+      if (this.fixed === false) {
+        return ''
+      }
+      return `m-tabbar__${this.fixed}`
+    },
+    safeBottom() {
+      return this.fixed === 'bottom' ? 'safe-area__block' : ''
+    }
+  },
+  watch: {
+    value(to, from) {
+      this.setActiveItem()
+    }
+  },
+  mounted() {
+    this.setActiveItem()
+  },
+
+  methods: {
+    setActiveItem() {
+      this.items.forEach((item, index) => {
+        item.isActive = index === this.value
+      })
+    },
+    onChange(active) {
+      // this.$emit('input', active)
+      this.$emit('change', active)
+    }
+  }
 }
 </script>
 <style lang="scss">
