cf2f54cf61b916fe4612cd65a68ba8b1189de4e4
zhouqing <448340778@qq.com>
2021-04-15 14:40:55 +0800

    fix: picker scrollEnd repeat

--- config/index.js
+++ config/index.js
@@ -79,7 +79,7 @@ module.exports = {
          */
 
         // https://webpack.js.org/configuration/devtool/#development
-        devtool: 'cheap-module-eval-source-map',
+        devtool: 'eval-source-map',
 
         // If you have problems debugging vue-files in devtools,
         // set this to false - it *may* help
